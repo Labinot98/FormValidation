48cd70aecc8ab2fe40f4342a0b4f189f901c0b8d
Labinot98 <labinotpajaziti0@gmail.com>
2022-11-08 23:48:03 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # FormValidation
 
-A description of this package.
+## Installation
+
+To install this package, import `url here` in SPM.
+
+## Usage example
+
+``` swift
+
+class RegisterViewController: UIViewController {
+    @IBOutlet weak var fullNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailAddressTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    var formValidationDelegate: FormValidationProtocol?
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            formValidationDelegate = FormValidation( 
+                                            button: signUpButton,
+                                            fields: [
+                                             fullNameTextField, lastNameTextField,
+                                            emailAddressTextField, passwordTextField
+                                            ],
+                                            handler: onSubmit
+                                        )
+        }
+        
+        private func onSubmit() {
+                // Make Action
+        }
+}
+
+```
--- Sources/FormValidation/FormValidation.swift
+++ Sources/FormValidation/FormValidation.swift
@@ -1,6 +1,56 @@
-public struct FormValidation {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import UIKit
 
-    public init() {
+ protocol FormValidationProtocol: AnyObject {
+    var button: UIButton { get set }
+    var fields: [UITextField] { get set }
+    var handler: () -> Void { get set }
+}
+
+class FormValidation: FormValidationProtocol {
+
+    var button: UIButton
+    var fields: [UITextField]
+    var handler: () -> Void
+    
+    init(button: UIButton, fields: [UITextField], handler: @escaping () -> Void) {
+        self.button = button
+        self.fields = fields
+        self.handler = handler
+        
+        button.addTarget(self, action: #selector(buttonClicked(_:)),for: .touchUpInside)
+        
+       fields.forEach {
+            $0.addTarget(self, action: #selector(checkFieldValidity(_:)), for: .editingChanged)
+            $0.addTarget(self, action: #selector(checkFieldValidity(_:)), for: .editingDidEnd)
+        }
     }
+    
+    @objc func buttonClicked(_ sender: AnyObject?) {
+        
+        fields.forEach(checkFieldValidity)
+        
+        let isFormValid = fields.allSatisfy({ isFieldValid(field: $0)})
+        
+        if(isFormValid) {
+            self.handler()
+        }
+    }
+    
+    @objc func checkFieldValidity(_ textField: UITextField) {
+        if !isFieldValid(field: textField) {
+            textField.layer.borderColor = UIColor.systemRed.cgColor
+        } else {
+            if(textField.isEditing) {
+                textField.layer.borderColor = UIColor.blue.cgColor
+            } else {
+                textField.layer.borderColor = UIColor.gray.cgColor
+            }
+        }
+    }
+    
+    func isFieldValid(field: UITextField) -> Bool {
+        return !(field.text?.isEmpty ?? true)
+    }
+    
 }
